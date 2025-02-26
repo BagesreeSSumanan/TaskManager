39fd510cfc3a8f68ea73372f6e647c25e6558b53
Bagesree <bagesree.s.sumanan@xminds.com>
2025-02-26 17:14:38 +0530

    task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { AddTaskComponent } from './dash-board/add-task/add-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashBoardComponent }, // Default route
+  { path: 'add-task', component: AddTaskComponent } // Ensure this route exists
+];
 
 @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { AddTaskComponent } from './dash-board/add-task/add-task.component';
+import { FormsModule } from '@angular/forms'; // ✅ Import FormsModule
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+    declarations: [
+      AppComponent,
+      DashBoardComponent,
+      AddTaskComponent  
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
